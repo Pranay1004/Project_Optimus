11edf3905f4f505dcae8665b146368bbffd73f3a
Pranay1004 <pranaypandey82@gmail.com>
2025-07-19 16:15:51 +0530

    Live Chalgaya localhost pe

--- tailwind.config.js
+++ tailwind.config.js
@@ -84,6 +84,18 @@ export default {
           '100%': { opacity: '1' },
         },
       },
+      safelist: [
+        'bg-blue-500',
+        'bg-green-500', 
+        'bg-purple-500',
+        'bg-yellow-500',
+        'bg-blue-600',
+        'bg-gray-600',
+        'bg-pink-500',
+        'bg-orange-500',
+        'bg-teal-500',
+        'bg-indigo-500'
+      ],
     },
   },
   plugins: [],
