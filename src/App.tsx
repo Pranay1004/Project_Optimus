b93827f212213de652bae5bd1078d4ddc29ad368
Pranay1004 <pranaypandey82@gmail.com>
2025-07-22 10:55:31 +0530

    Latest on 22 July

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
-import BeforeAfter from './components/BeforeAfter';
+// import BeforeAfter from './components/BeforeAfter';
 import Testimonials from './components/Testimonials';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
@@ -17,7 +17,7 @@ function App() {
       <Hero />
       <About />
       <Services />
-      <BeforeAfter />
+      {/* <BeforeAfter /> */}
       <Testimonials />
       <ContactForm />
       <Footer />
